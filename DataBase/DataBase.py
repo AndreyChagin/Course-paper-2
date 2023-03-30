a6db7e5b063abce1d61423ace8e7a878e02b98b4
AndreyChagin <volchonok1337@rambler.ru>
2023-03-30 23:23:55 +0300

    course paper

--- DataBase/DataBase.py
+++ DataBase/DataBase.py
@@ -3,4 +3,4 @@ from pyodbc import connect
 
 class DataBase:
     def __init__(self):
-        self.conn = connect("Driver={SQL Server}; Server=DESKTOP-E1HH3A6; Trusted_Connection=True; Database=course")
+        self.conn = connect("Driver={SQL Server}; Server=D*SKTOP-E1HH3A6; Trusted_Connection=True; Database=course")
