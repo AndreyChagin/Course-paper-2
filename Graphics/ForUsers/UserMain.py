dba3a82672fdd647f8c3a76f2d8844faf30d2fde
AndreyChagin <volchonok1337@rambler.ru>
2023-04-07 18:33:26 +0300

    course paper

--- Graphics/ForUsers/UserMain.py
+++ Graphics/ForUsers/UserMain.py
@@ -10,11 +10,11 @@ class User:
 
     def __init__(self):
         self.__app = CTk()
-        self.__app.geometry('300x300')
+        self.__app.geometry('300x225')
         self.__app.resizable(0, 0)
         self.__app.title('Выбор')
 
-        self.__frame = customtkinter.CTkFrame(master=self.__app, height=230, corner_radius=15, border_color='gray',
+        self.__frame = customtkinter.CTkFrame(master=self.__app, height=175, corner_radius=15, border_color='gray',
                                               border_width=1)
         self.__button_app = customtkinter.CTkButton(
             master=self.__frame,
@@ -42,5 +42,5 @@ class User:
     def loop(self):
         self.__frame.place(x=50, y=20)
         self.__button_app.place(x=30, y=50)
-        self.__button_service.place(x=30, y=150)
+        self.__button_service.place(x=30, y=100)
         self.__app.mainloop()
